3c2d9babd78e0563eb6ed3c11687b55ddcf2af41
Axel Velasquez <yoloax11@gmail.com>
2025-06-29 13:53:22 -0700

    added the db connection

---
+++ backend/Database/db.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+
+        if( !process.env.MONGODB_URI ) {
+            throw new Error('MONGODB_URI is not defined');
+        }
+
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error('❌ Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
