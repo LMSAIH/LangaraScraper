4578dbd05994ed60058c7765c7c832492e258a49
Axel Velasquez <yoloax11@gmail.com>
2025-06-29 17:05:03 -0700

    added types

--- backend/Types/ScraperTypes.ts
+++ backend/Types/ScraperTypes.ts
@@ -3,3 +3,28 @@ export interface BCTransferSubject {
   Code: string;
   Title: string;
 }
+
+export interface CourseSection {
+  crn: string;
+  subject: string;
+  course: string;
+  section: string;
+  credits: string;
+  title: string;
+  type: string;
+  days: string;
+  time: string;
+  room: string;
+  instructor: string;
+  seatsAvailable: string;
+  waitlist: string;
+  additionalFees: string;
+  repeatLimit: string;
+  notes?: string;
+}
+
+export interface CourseData {
+  courseCode: string;
+  subject: string;
+  sections: CourseSection[];
+}
