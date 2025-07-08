126c892b35f01e23f9f54e875ec06379b9c5fb2b
Axel Velasquez <yoloax11@gmail.com>
2025-07-08 13:17:58 -0700

    added the historical data route

--- backend/routes/Scraper/Courses.ts
+++ backend/routes/Scraper/Courses.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import * as cron from "node-cron";
 import {
   handleGetCourses,
   handleGetSubjects,
+  handleGetAllHistoricalCourses
 } from "../../Controllers/CourseScraper";
 
 const router = Router();
 
 router.get("/subjects", handleGetSubjects);
 router.post("/courses", handleGetCourses);
+router.post("/courses/historical", handleGetAllHistoricalCourses);
 
 export { router };
